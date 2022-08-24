cda2724a27918b62a893ffc00b191b6f9251614e
User <elmuratuulua@gmail.com>
2022-08-24 16:00:28 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        UnaryOperator <Double> srood;
+        srood = (a) -> Math.sqrt(a);
+        System.out.println(srood.sprt(16.0));
+    }
+}
---
+++ src/UnaryOperator.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface UnaryOperator <T> {
+    double sprt(T number);
+}
